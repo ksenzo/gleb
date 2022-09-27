a7b79b5f23ecfa794732915defb2448a56aeef62
ksenzo <ksenzo.work@gmail.com>
2022-09-27 15:37:50 +0300

    fiч html connect

--- bot/settings.py
+++ bot/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-+bequklj642z=)g+@l3-gz$&y_qj+kppoarfo$qq2oa%yzpo*k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ksenzo.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = ['ksenzo.pythonanywhere.com',]
 
 
 # Application definition
