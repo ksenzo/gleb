c97df80548ad03d93b632e7fe745e4bf5bb08d49
ksenzo <ksenzo.work@gmail.com>
2022-09-28 23:12:38 +0300

    finish game page

--- static/js/main.js
+++ static/js/main.js
@@ -221,45 +221,20 @@ function selectChest(choice) {
                 let firstChestOpenedLose = firstChest.getAttribute("data-original-lose")
                 let secondChestOpenedLose = secondChest.getAttribute("data-original-lose")
 
-
-                // if (result.winning === 'game_winning') {
-                //     $('#select_chest').text('Вы выиграли!')
-                //     if (choice === 'left') {
-                //         $("#game_result_left").text(result.winning_amount)
-                //         $("#game_result_right").text(0)
-                //     } else if (choice === 'right') {
-                //         $("#game_result_left").text(0)
-                //         $("#game_result_right").text(result.winning_amount)
-                //     }
-                // } else if (result.winning === 'game_loosing') {
-                //     $('#select_chest').text('Повезёт в следующий раз')
-                //     if (choice === 'left') {
-                //         $("#game_result_left").text(0)
-                //         $("#game_result_right").text(result.winning_amount)
-                //     } else if (choice === 'right') {
-                //         $("#game_result_left").text(result.winning_amount)
-                //         $("#game_result_right").text(0)
-                //     }
-                // }
-
                 if (result.winning === 'game_winning') {
-                    $('#select_chest').text('Вы выиграли!')
                     $("#game_result").append(
                         `<div style="display: flex; flex-direction: column; align-items: center;">
                                 <span class="win_font">ПОБЕДА!</span>
-                                <span class="win_font">${result.winning_amount}</span>
-                            </div>`
+                                <span class="win_font">${result.winning_amount} UAH</span>
+                        </div>`
                     )
                     if (choice === 'left') {
                         firstChest.setAttribute('src', firstChestOpenedWin);
-                        secondChest.setAttribute('src', secondChestOpenedLose);
                     } else if (choice === 'right') {
-                        firstChest.setAttribute('src', firstChestOpenedLose);
                         secondChest.setAttribute('src', secondChestOpenedWin);
 
                     }
                 } else if (result.winning === 'game_loosing') {
-                    $('#select_chest').text('Повезёт в следующий раз')
                     $("#game_result").append(
                         `<div style="display: flex; flex-direction: column; align-items: center;">
                                 <span class="win_font">ПОПРОБУЙ ЕЩЕ РАЗ!</span>
@@ -267,9 +242,7 @@ function selectChest(choice) {
                     )
                     if (choice === 'left') {
                         firstChest.setAttribute('src', firstChestOpenedLose);
-                        secondChest.setAttribute('src', secondChestOpenedWin);
                     } else if (choice === 'right') {
-                        firstChest.setAttribute('src', firstChestOpenedWin);
                         secondChest.setAttribute('src', secondChestOpenedLose);
                     }
                 }
@@ -278,7 +251,9 @@ function selectChest(choice) {
 
                 $('#chest_1').attr("onclick", "")
                 $('#chest_2').attr("onclick", "")
-                var newGameButton = `<button class="button_new_game" onclick="window.location.reload();">Новая игра</button>`
+                var newGameButton = `<div class="new_game_render_wrap">
+                                        <button class="button_new_game btn_start_game" onclick="window.location.reload();">Новая игра</button>
+                                    </div>`
                 // let repeatOnclick = `repeatBetGame(${winning_amount / 2});`
                 // var repeatBetButton = `<button id="repeatBetButton" class="button_new_game">Повторить<br>ставку</button>`
                 $("#game_to_start").append(newGameButton);
@@ -289,6 +264,8 @@ function selectChest(choice) {
         }
     })
 }
+
+//selectChest('left');
 
 function repeatBetGame(amount) {
     $.ajax({
@@ -307,8 +284,6 @@ function repeatBetGame(amount) {
                             $("#chest_1").attr("class", "chest");
                             $("#chest_2").attr("class", "chest");
                             $("#chose_bet_size").hide();
-
-
                             $('#select_chest').text('Выберите сундук')
                             $('#chest_1').attr("onclick", "selectChest('left')")
                             $('#chest_2').attr("onclick", "selectChest('right')")
@@ -355,8 +330,6 @@ function handlePlusDep() {
         deposit_amount.value = parseInt(deposit_amount.value) + 5000
     }
 }
-
-//selectChest('left');
 
 function setAmountDep(deposit_amount_value) {
     deposit_amount.value = deposit_amount_value
