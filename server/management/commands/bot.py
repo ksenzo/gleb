cc775e25d65cf48b7d41b2db72cec9d086460e85
ksenzo <ksenzo.work@gmail.com>
2022-10-07 19:50:52 +0300

    test bonus bag

--- server/management/commands/bot.py
+++ server/management/commands/bot.py
@@ -64,10 +64,6 @@ class Command(BaseCommand):
             bot.send_message(message.chat.id, mess, parse_mode='html', reply_markup=markup)
 
         def start_game(message):
-            user = User.objects.get(telegram_id=message.from_user.id)
-            wallet = Wallet.objects.get(owner=user)
-            wallet.balance = 0
-            wallet.save()
             markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
             start_game = types.KeyboardButton('🎲Начать игру')
             balance = types.KeyboardButton('💰Баланс')
